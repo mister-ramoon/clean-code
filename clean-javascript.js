47beeb3ec0b31c68dbed4bf9804efcc84fb505c1
ramonruiz <jruiz@lightia.mx>
2023-05-04 14:32:26 -0600

    Cómo nombrar según el tipo de dato

--- clean-javascript.js
+++ clean-javascript.js
@@ -1,11 +1,59 @@
+/*Arrays*/
+
 // No recomendado
-const yyyymmdstr = moment().format("YYYY/MM/DD");
-let arrayNames = ["Diego", "Karen", "Oscar"];
-getUserInfo();
-getClientData();
-getCustomerRecord();
+const user = ["Juan", "Mariana", "Carmen"];
+
+// Regular
+const userList = ["Juan", "Mariana", "Carmen"];
+
+// Bueno
+const users = ["Juan", "Mariana", "Carmen"];
+
+// Excelente
+const usersNames = ["Juan", "Mariana", "Carmen"];
+
+/*Booleanos*/
+
+// No recomendado
+const valid = true;
+const read = false;
+const color = true;
 
 // Recomendado
-const currentDate = moment().format("YYYY/MM/DD");
-let nameList = ["Diego", "Karen", "Oscar"];
-getUser();
+const isValid = true;
+const canRead = false;
+const hasColor = true;
+
+/*Números*/
+
+// No recomendado
+const users = 15;
+
+// Recomendado
+const maxUsers = 20;
+const minUsers = 10;
+const totalUsers = 15;
+
+/*Funciones*/
+
+// No recomendado
+createUserIfNotExists();
+updateUserIfNotExists();
+sendEmailIfIsValid();
+
+// Recomendado
+createUser();
+updateUser();
+sendEmail();
+
+/*Clases*/
+
+// No recomendado
+class Data {}
+class Manager {}
+class Infor {}
+
+// Recomendado
+class User {}
+class UserAccount {}
+class UserInformation {}
