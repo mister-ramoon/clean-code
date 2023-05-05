75d6f7510e97f5eb25942ae29302dd5b098b9f61
ramonruiz <jruiz@lightia.mx>
2023-05-05 11:14:53 -0600

    Tamaño reducido (responsabilidad única)

--- clean-javascript.js
+++ clean-javascript.js
@@ -1,52 +1,25 @@
-// Clase Padre antes de ES6
-function Person(name) {
-  this.name = name;
-}
-
-Person.prototype.greet = function () {
-  return "Hello, my name is " + this.name;
-};
-
-// Clase Hija antes de ES6
-function Developer(name) {
-  this.name = name;
-}
-
-Developer.prototype = Object.create(Person.prototype);
-
-Developer.prototype.writeCode = function (coffee) {
-  if (coffee) console.log("I am working in a new feature");
-  else console.log("I need coffee!");
-};
-
-var dev = new Developer("Jorge");
-console.log(dev.greet());
-dev.writeCode(true); // I am working in a new feature
-
-// Clase Padre con ES6
-class Person {
-  constructor(name) {
-    this.name = name;
+class UserAuth {
+  constructor(user) {
+    this.user = user;
   }
 
-  greet() {
-    return `Hello, my name is ${this.name}`;
+  verifyCredentials() {
+    return true;
   }
 }
 
-// Clase Hija con ES6
-class Developer extends Person {
-  constructor(name) {
-    super(name);
+class UserSettings extends UserAuth {
+  constructor(settings) {
+    super(user);
+    this.settings = settings;
   }
 
-  writeCode(coffee) {
-    coffee
-      ? console.log("I am working in a new feature")
-      : console.log("I need coffee!");
+  changeSettings(settings) {
+    if (this.verifyCredentials()) {
+      console.log("Puede cambiar la configuración");
+    }
   }
 }
 
-const dev = new Developer("Jorge");
-console.log(dev.greet());
-dev.writeCode(true); // I am working in a new feature
+const newAccess = new UserSettings("Juan", "DarkMode");
+newAccess.changeSettings();
