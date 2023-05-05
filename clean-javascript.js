4e6960f6a9965db3aa6203092ed9129bdff76afb
ramonruiz <jruiz@lightia.mx>
2023-05-05 09:39:45 -0600

    Declaración y expresión de funciones

--- clean-javascript.js
+++ clean-javascript.js
@@ -1,59 +1,14 @@
-// Ámbito global
-let greeting = "Hello World";
+// Declaración de funciones
 
-function greet() {
-  console.log(greeting);
+function getResult() {
+  return "Result";
 }
 
-greet(); // Hello World
+getResult(); // "Result"
 
-// Ámbito local o de función
-function greet() {
-  let greeting = "Hello World";
-  console.log("Saludo desde la función", greeting);
-}
+// Exoression function
+const getResult = function () {
+  return "Result";
+};
 
-greet(); // Hello World
-console.log(greeting);
-
-// Ámbito de bloque
-{
-  let greeting = "Hello World";
-  var lang = "English";
-
-  console.log(greeting); // Hello World
-}
-
-console.log(lang); // English
-console.log(greeting); // ReferenceError: greeting is not defined
-
-// Ámbito estático o lexico
-const age = 20;
-
-function printAge() {
-  console.log(age);
-}
-
-function mainApp() {
-  const age = 30;
-  printAge();
-}
-
-mainApp(); // 20
-
-// Hoisting
-greet(); // Hello World
-
-function greet() {
-  let greeting = "Hello World";
-  console.log(greeting);
-}
-
-var greed = "Hello";
-
-(function () {
-  var greed;
-  console.log(greed);
-  var greed = "Hi";
-  console.log(greed);
-})();
+getResult(); // "Result"
