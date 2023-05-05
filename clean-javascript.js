72ad59a13bc1b8cce09c288a97f8bca3a46ef0aa
ramonruiz <jruiz@lightia.mx>
2023-05-05 10:03:04 -0600

    Parámetros y argumentos

--- clean-javascript.js
+++ clean-javascript.js
@@ -1,14 +1,43 @@
-// Declaración de funciones
-
-function getResult() {
-  return "Result";
+// Parameters: number
+function getDouble(number) {
+  return number * 2;
 }
 
-getResult(); // "Result"
+// Arguments: 3
+getDouble(3); // 6
 
-// Exoression function
-const getResult = function () {
-  return "Result";
+// Default parameters
+function greet(person = "stranger") {
+  return `Hello, ${person}!`;
+}
+
+greet(); // "Hello, stranger!"
+greet("John"); // "Hello, John!"
+
+// Rest parameters
+function add(...args) {
+  console.log(args.reduce((prev, current) => prev + current, 0));
+}
+
+add(1, 2, 3, 4, 5); // 15
+
+// Spread operator
+const course = {
+  title: "JavaScript",
+  content: "Learn JavaScript",
 };
 
-getResult(); // "Result"
+const courseCloned = Object.assign({}, course);
+const courseClonedSpread = { ...course };
+console.log(courseClonedSpread); // { title: "JavaScript", content: "Learn JavaScript" }
+
+const numbers = [1, 2, 3, 4, 5];
+
+const numbersCloned = numbers.slice();
+const numbersClonedSpread = [...numbers];
+
+const numberrsA = [1, 2, 3, 4, 5];
+const numberrsB = [6, 7, 8, 9, 10];
+
+const numbersConcat = numberrsA.concat(numberrsB);
+const numbersConcatSpread = [...numberrsA, ...numberrsB];
