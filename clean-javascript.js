8f046622ed318de244e68f13740e0f1bbe1bd988
ramonruiz <jruiz@lightia.mx>
2023-05-05 10:13:14 -0600

    POO con ES6 (constructores, métodos y herencia)

--- clean-javascript.js
+++ clean-javascript.js
@@ -1,43 +1,27 @@
-// Parameters: number
-function getDouble(number) {
-  return number * 2;
+// Antes de ES6
+function Person(name) {
+  this.name = name;
 }
 
-// Arguments: 3
-getDouble(3); // 6
-
-// Default parameters
-function greet(person = "stranger") {
-  return `Hello, ${person}!`;
-}
-
-greet(); // "Hello, stranger!"
-greet("John"); // "Hello, John!"
-
-// Rest parameters
-function add(...args) {
-  console.log(args.reduce((prev, current) => prev + current, 0));
-}
-
-add(1, 2, 3, 4, 5); // 15
-
-// Spread operator
-const course = {
-  title: "JavaScript",
-  content: "Learn JavaScript",
+Person.prototype.greet = function () {
+  return "Hello, my name is " + this.name;
 };
 
-const courseCloned = Object.assign({}, course);
-const courseClonedSpread = { ...course };
-console.log(courseClonedSpread); // { title: "JavaScript", content: "Learn JavaScript" }
+var person = new Person("John");
+console.log(person.name); // John
+console.log(person.greet()); // Hello, my name is John
 
-const numbers = [1, 2, 3, 4, 5];
+// ES6
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
 
-const numbersCloned = numbers.slice();
-const numbersClonedSpread = [...numbers];
+  greet() {
+    return "Hello, my name is " + this.name;
+  }
+}
 
-const numberrsA = [1, 2, 3, 4, 5];
-const numberrsB = [6, 7, 8, 9, 10];
-
-const numbersConcat = numberrsA.concat(numberrsB);
-const numbersConcatSpread = [...numberrsA, ...numberrsB];
+let person = new Person("John");
+console.log(person.name); // John
+console.log(person.greet()); // Hello, my name is John
